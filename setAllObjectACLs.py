6b2e5700bcfb85c8601c7e5341946fefbec13e5d
microtodd <todd.davenport@yahoo.com>
2017-07-31 12:32:33 -0500

    Create setAllObjectACLs.py

---
+++ setAllObjectACLs.py
@@ -0,0 +1,48 @@
+import boto3
+import sys
+import getopt
+
+# Globals
+BucketName = ''
+Env = ''
+ProfileName = 'default'
+NewACL = 'authenticated-read'
+
+# Parse CL opts
+opts, args = getopt.getopt(sys.argv[1:],'he:b:p:a:')
+for opt, arg in opts:
+    if opt == '-h':
+        print '-e envname'
+        print '-b bucketname'
+        print '-p profile name'
+        print '-a ACL (authenticated-read)'
+        sys.exit(0)
+    if opt == '-e':
+        Env = str(arg)
+    if opt == '-b':
+        BucketName = str(arg)
+
+# Get applicable creds
+mySession = boto3.Session(profile_name=ProfileName)
+
+# Get all the objects
+allS3Keys = []
+s3Client = mySession.client('s3')
+nextToken = 1
+while nextToken is not None:
+    allObjects = None
+    if nextToken == 1:
+        allObjects = s3Client.list_objects_v2(Bucket=BucketName)
+    else:
+        allObjects = s3Client.list_objects_v2(Bucket=BucketName,ContinuationToken=nextToken)
+    if allObjects.get('IsTruncated') == True:
+        nextToken = allObjects.get('NextContinuationToken')
+    else:
+        nextToken = None
+
+    for i in allObjects['Contents']:
+        allS3Keys.append(str(i['Key']))
+
+# Now set the ACL for each object
+for key in allS3Keys:
+    s3Client.put_object_acl(ACL=NewACL,Bucket=BucketName,Key=key)
